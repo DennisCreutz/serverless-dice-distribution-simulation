ab0809eb9c018f209f59e53bbff809611db696ba
dcreutz <dennis.creutz@prodyna.com>
2021-12-08 17:34:47 +0100

    Removed API Gateway module.

--- modules/api-gateway/lambda-proxy/outputs.tf
+++
@@ -1,3 +0,0 @@
-output "api_gateway_deploy_trigger" {
-  value = concat(aws_api_gateway_method.this.*.id, aws_api_gateway_integration.this.*.id, aws_api_gateway_integration_response.http_200.*.id, aws_api_gateway_method_response.http_200.*.id)
-}
